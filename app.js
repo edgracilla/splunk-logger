daac407448828dc765445fc53ce46a1e20f310f2
akzReekoh <achilles.dinglasan@p3ople4u.com>
2015-12-03 16:48:00 +0800

    REEKOH-278
    -Finished code (not tested yet)
    -Finished documentation

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var platform = require('./platform'),
+    isPlainObject = require('lodash.isplainobject'),
+	splunkClient;
+
+platform.on('log', function (logData) {
+    if(!logData) return;
+
+    if(isPlainObject(logData))
+        logData = JSON.stringify(logData);
+
+    splunkClient.send(logData, function(error, response, body) {
+        if(error){
+            console.error(error);
+            platform.handleException(error);
+        }
+    });
+});
+
+platform.once('close', function () {
+    platform.notifyClose();
+});
+
+platform.once('ready', function (options) {
+    var Splunk = require('splunk-logging').Logger;
+    splunkClient = new Splunk({token: options.token, url: options.url});
+
+	platform.notifyReady();
+});
