1b5061258d78ffc822b6e2c0ed90cd609eade6ab
edgracilla <ed.gracilla@gmail.com>
2017-03-28 18:06:33 +0800

    update

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 'use strict'
 
 const reekoh = require('reekoh')
-const _plugin = new reekoh.plugins.Logger()
+const plugin = new reekoh.plugins.Logger()
 const isEmpty = require('lodash.isempty')
 
 let splunkClient = null
 
-_plugin.on('log', (logData) => {
+plugin.on('log', (logData) => {
   if (isEmpty(logData)) {
-    _plugin.logException(new Error(`Invalid data received. Data must not be empty.`))
+    plugin.logException(new Error(`Invalid data received. Data must not be empty.`))
   } else {
     splunkClient.send({message: logData}, (error) => {
       if (error) {
         console.error('Error on Splunk', error)
-        _plugin.logException(error)
+        plugin.logException(error)
       }
     })
-    _plugin.log(JSON.stringify({
+    plugin.log(JSON.stringify({
       title: 'Log sent to Splunk',
       data: logData
     }))
   }
 })
 
-_plugin.once('ready', () => {
+plugin.once('ready', () => {
   const Splunk = require('splunk-logging').Logger
-  splunkClient = new Splunk({token: _plugin.config.token, url: _plugin.config.url})
+  splunkClient = new Splunk({token: plugin.config.token, url: plugin.config.url})
 
-  _plugin.log('Splunk Logger has been initialized.')
-  _plugin.emit('init')
+  plugin.log('Splunk Logger has been initialized.')
+  plugin.emit('init')
 })
 
-module.exports = _plugin
+module.exports = plugin
